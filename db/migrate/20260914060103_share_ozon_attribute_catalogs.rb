b42babc9d84890dde0abce213e0943c8dc1cbfe4
peter.ji <583255925@qq.com>
2026-09-14 16:25:40 +0800

    Make Ozon catalog migration tolerant

--- db/migrate/20260914060103_share_ozon_attribute_catalogs.rb
+++ db/migrate/20260914060103_share_ozon_attribute_catalogs.rb
@@ -4,16 +4,16 @@ class ShareOzonAttributeCatalogs < ActiveRecord::Migration[8.1]
     # shared-key model instead of carrying forward the old account-scoped data.
     execute "TRUNCATE TABLE raw_ozon_product_attributes, raw_ozon_attribute_values, raw_ozon_category_attributes"
 
-    remove_foreign_key :raw_ozon_category_attributes, column: :account_id
-    remove_foreign_key :raw_ozon_attribute_values, column: :account_id
-    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_cat_attrs_unique"
-    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_cat_attrs_category_type"
-    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_cat_attrs_dictionary"
-    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_category_attributes_on_account_id"
-    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attr_values_unique"
-    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attr_values_attribute"
-    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attr_values_value"
-    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attribute_values_on_account_id"
+    remove_foreign_key :raw_ozon_category_attributes, column: :account_id, if_exists: true
+    remove_foreign_key :raw_ozon_attribute_values, column: :account_id, if_exists: true
+    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_cat_attrs_unique", if_exists: true
+    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_cat_attrs_category_type", if_exists: true
+    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_cat_attrs_dictionary", if_exists: true
+    remove_index :raw_ozon_category_attributes, name: "idx_raw_ozon_category_attributes_on_account_id", if_exists: true
+    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attr_values_unique", if_exists: true
+    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attr_values_attribute", if_exists: true
+    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attr_values_value", if_exists: true
+    remove_index :raw_ozon_attribute_values, name: "idx_raw_ozon_attribute_values_on_account_id", if_exists: true
 
     remove_column :raw_ozon_category_attributes, :account_id, :bigint
     remove_column :raw_ozon_attribute_values, :account_id, :bigint
